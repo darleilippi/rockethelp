f6a7b3ac4577483ac98d23e5e75aaca380f74a17
Darlei Lippi <lippidev@gmail.com>
2022-07-19 23:15:13 -0300

    Adição da funcionalidade de registrar uma solicitação

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import { VStack } from 'native-base';
-import { Button } from '../components/Button';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import firestore from '@react-native-firebase/firestore'
+
+import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 
 export function Register() {
+    const [loading, setLoading] = useState(false)
+    const [patrimony, setPatrimony] = useState('')
+    const [description, setDescription] = useState('')
+
+    const navigation = useNavigation()
+
+    function handleNerOrderRegister() {
+        if (!patrimony || !description) {
+            return Alert.alert('Registrar', 'Preencha todos os campos.')
+        }
+
+        setLoading(true)
+
+        firestore()
+            .collection('orders')
+            .add({
+                patrimony,
+                description,
+                status: 'open',
+                created_at: firestore.FieldValue.serverTimestamp()
+            })
+            .then(() => {
+                Alert.alert('Solicitação', 'Solicitação registrada com sucesso.')
+                navigation.goBack()
+            })
+            .catch((error) => {
+                console.log(error)
+                setLoading(false)
+                return Alert.alert('Registrar', 'Ocorreu um erro ao registrar.')
+            })
+    }
+
     return (
         <VStack flex={1} p={6} bg="gray.600">
             <Header title="Nova solicitação" />
@@ -13,6 +50,7 @@ export function Register() {
                 placeholder='Número do patrimônio'
                 mt={4}
                 textAlignVertical='top'
+                onChangeText={setPatrimony}
             />
 
             <Input 
@@ -21,11 +59,14 @@ export function Register() {
                 mt={5}
                 multiline
                 textAlignVertical='top'
+                onChangeText={setDescription}
             />
 
             <Button 
                 title='Cadastrar'
                 mt={5}
+                isLoading={loading}
+                onPress={handleNerOrderRegister}
             />
         </VStack>
     );
